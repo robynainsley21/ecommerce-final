ab231045ddd00e0080f612ee3740b3d6e550a76a
robynainsley21 <robynainsley2i@gmail.com>
2024-06-11 13:42:19 +0200

    Edit file

--- script/checkout.js
+++ script/checkout.js
@@ -1,1 +1,6 @@
-import { checkoutProducts } from "./product";
+// import { displayProducts, allProducts } from "./product";
+
+// displayProducts(allProducts)
+
+// console.log(`all products: ${allProducts}`);
+
