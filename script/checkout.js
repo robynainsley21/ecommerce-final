419c3e5e64bd4ee112001afa92601729c49217b6
robynainsley21 <robynainsley2i@gmail.com>
2024-06-11 16:11:58 +0200

    Complete checkout page

--- script/checkout.js
+++ script/checkout.js
@@ -1,46 +1,76 @@
-
-const checkoutItems = JSON.parse(localStorage.getItem("checkout")) || []
-const checkoutBox = document.querySelector('[checkout-content]')
-const payBtn = document.querySelector('[purchase-btn]')
+const checkoutItems = JSON.parse(localStorage.getItem("checkout")) || [];
+const checkoutBox = document.querySelector("[checkout-content]");
+const payBtn = document.querySelector("[purchase-btn]");
+const allTotal = document.querySelector("[all-total]");
 
 console.log(checkoutItems);
+const productQuantity = {};
 
-checkoutItems.forEach(product => {
-    checkoutBox.innerHTML += `
-        <tr class="selected_prod" id="${product.id}">
-            <th scope="row"><img src="${product.img_url}" alt="checkout-img"/></th>
-            <td>${product.productName}</td>
-            <td>R${product.price}</td>
-            <td>insert quantity here</td>
-            <td>total here</td>
-            <td><button class="btn btn-danger" onclick="removeItem(${product.id})">Remove</button></td>
-        </tr>
-    `
-})
+checkoutItems.forEach((product) => {
+  /**if the product is already in the object */
+  if (productQuantity[product.id]) productQuantity[product.id].quantity++;
+  else productQuantity[product.id] = { product, quantity: 1 };
+});
 
-function removeItem(itemId){
-    try {
-        console.log(`this item ${itemId} was removed`);
-        let deletedProdIndex = checkoutItems.findIndex(product => product.id === itemId)
-    
-        if(deletedProdIndex !== -1){
-            checkoutItems.splice(deletedProdIndex, 1)
-            localStorage.setItem("checkout", JSON.stringify(checkoutItems))
-            const removedRow = document.getElementById(itemId)
-            removedRow.parentNode.removeChild(removedRow)
-        }
-    } catch (error) {
-        alert('Item could not be removed. Please try again.')
-        console.error("Item could not be removed", error);
+let totalPrice = 0;
+
+Object.values(productQuantity).forEach(({ product, quantity }) => {
+  const price = quantity * parseInt(product.price);
+  totalPrice += price;
+  checkoutBox.innerHTML += `
+    <tr class="selected_prod" id="${product.id}">
+      <th scope="row"><img src="${product.img_url}" alt="checkout-img"/></th>
+      <td>${product.productName}</td>
+      <td>${product.category}</td>
+      <td>R${product.price}</td>
+      <td>${quantity}</td>
+      <td>R${price}</td>
+      <td><button class="btn btn-danger" onclick="removeItem(${product.id})">Remove</button></td>
+    </tr>
+  `;
+  console.log(product);
+});
+
+allTotal.textContent += `R${totalPrice}`;
+
+function removeItem(itemId) {
+  try {
+    console.log(`this item ${itemId} was removed`);
+    let deletedProdIndex = checkoutItems.findIndex(
+      (product) => product.id === itemId
+    );
+
+    if (deletedProdIndex !== -1) {
+      checkoutItems.splice(deletedProdIndex, 1);
+      localStorage.setItem("checkout", JSON.stringify(checkoutItems));
+      const removedRow = document.getElementById(itemId);
+      removedRow.parentNode.removeChild(removedRow);
+
+      const productToRemove = productQuantity[itemId];
+      if (productToRemove.quantity > 1) {
+        productToRemove.quantity--;
+      } else {
+        delete productQuantity[itemId];
+      }
+
+      /**when a product gets removed, price to be decreased */
+      let newTotalPrice = 0;
+      Object.values(productQuantity).forEach(({ product, quantity }) => {
+        newTotalPrice += quantity * parseInt(product.price);
+      });
+
+      allTotal.textContent += `R${newTotalPrice}`;
     }
-
+  } catch (error) {
+    alert("Item could not be removed. Please try again.");
+    console.error("Item could not be removed", error);
+  }
 }
 
-payBtn.addEventListener('click', () => {
-    if(checkoutItems.length > 0){
-        alert("Thank you for purchasing.")
-    }else{
-        alert("Your cart is empty. Please add a product.")
-    }
-})
-
+payBtn.addEventListener("click", () => {
+  if (checkoutItems.length > 0) {
+    alert("Thank you for purchasing.");
+  } else {
+    alert("Your cart is empty. Please add a product.");
+  }
+});
